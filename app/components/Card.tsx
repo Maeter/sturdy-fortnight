8f2954a6715f84b7f32e548cdfc30a841a559ca7
Luis Miguel Fernandez <lmfernandezb@gmail.com>
2026-05-13 22:33:59 +0200

    chore: add prettier and lint-staged

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,25 +1,15 @@
 type CardProps = {
-  title?: React.ReactNode;
-  footer?: React.ReactNode;
-  children?: React.ReactNode;
-};
+  title?: React.ReactNode
+  footer?: React.ReactNode
+  children?: React.ReactNode
+}
 
 export default function Card({ title, footer, children }: CardProps) {
   return (
-    <div className="w-full max-w-md rounded-2xl bg-white shadow-md overflow-hidden">
-      {title && (
-        <div className="px-6 py-4">
-          {title}
-        </div>
-      )}
-      <div className="px-6 py-4">
-        {children}
-      </div>
-      {footer && (
-        <div className="px-6 py-4 flex gap-2">
-          {footer}
-        </div>
-      )}
+    <div className="w-full max-w-md overflow-hidden rounded-2xl bg-white shadow-md">
+      {title && <div className="px-6 py-4">{title}</div>}
+      <div className="px-6 py-4">{children}</div>
+      {footer && <div className="flex gap-2 px-6 py-4">{footer}</div>}
     </div>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,30 @@
-import Card from "./components/Card";
+import Card from './components/Card'
 
 export default function Home() {
   return (
     <main className="flex flex-1 items-center justify-center bg-linear-to-r from-sky-300 to-cyan-100">
       <Card
-        title={<h2 className="text-lg font-semibold text-zinc-800">This is a technical proof</h2>}
+        title={
+          <h2 className="text-lg font-semibold text-zinc-800">
+            This is a technical proof
+          </h2>
+        }
         footer={
           <>
-            <button className="px-4 py-2 rounded-lg text-blue-800 border border-blue-800 text-sm ">
+            <button className="rounded-lg border border-blue-800 px-4 py-2 text-sm text-blue-800">
               Undo
             </button>
-            <button className="px-4 py-2 rounded-lg border border-blue-800 text-sm text-blue-800">
+            <button className="rounded-lg border border-blue-800 px-4 py-2 text-sm text-blue-800">
               Add
             </button>
-            <button className="px-4 py-2 rounded-lg bg-blue-800 text-white text-sm">
+            <button className="rounded-lg bg-blue-800 px-4 py-2 text-sm text-white">
               Delete
             </button>
           </>
         }
       >
-        <p className="text-zinc-500 text-sm">Your TODO list goes here.</p>
+        <p className="text-sm text-zinc-500">Your TODO list goes here.</p>
       </Card>
     </main>
-  );
+  )
 }
